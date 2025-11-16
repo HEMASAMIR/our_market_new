663337fd69031f8bf42aaed392fe358cdf855339
HEMASAMIR <ibrahimsameer78@apple.com>
2025-11-16 16:43:23 +0200

    success to setup package paymob

--- lib/core/components/product_card.dart
+++ lib/core/components/product_card.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:our_market/core/app_colors.dart';
 import 'package:our_market/core/components/cache_image.dart';
@@ -5,13 +6,15 @@ import 'package:our_market/core/functions/navigate_to.dart';
 import 'package:our_market/core/models/product_model/product_model.dart';
 import 'package:our_market/views/auth/ui/widgets/custom_elevated_btn.dart';
 import 'package:our_market/views/product_details/ui/product_details_view.dart';
+import 'package:pay_with_paymob/pay_with_paymob.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard(
-      {super.key,
-      required this.productModel,
-      required this.onPressed,
-      required this.isFavourite});
+  const ProductCard({
+    super.key,
+    required this.productModel,
+    required this.onPressed,
+    required this.isFavourite,
+  });
   final ProductModel productModel;
   final void Function()? onPressed;
   final bool isFavourite;
@@ -20,110 +23,132 @@ class ProductCard extends StatelessWidget {
     return GestureDetector(
       onTap: () {},
       child: Card(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-          ),
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      navigateTo(
-                          context, ProductDetailsView(product: productModel));
-                    },
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    navigateTo(
+                      context,
+                      ProductDetailsView(product: productModel),
+                    );
+                  },
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                      bottomLeft: Radius.circular(16),
+                    ),
+                    child: CaheImage(
+                      url:
+                          productModel.imageUrl ??
+                          "https://img.freepik.com/premium-psd/kitchen-product-podium-display-background_1101917-13418.jpg?w=900",
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 65,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      color: AppColors.kPrimaryColor,
+                      borderRadius: BorderRadius.only(
                         topRight: Radius.circular(16),
                         bottomRight: Radius.circular(16),
-                        bottomLeft: Radius.circular(16),
-                      ),
-                      child: CaheImage(
-                        url: productModel.imageUrl ??
-                            "https://img.freepik.com/premium-psd/kitchen-product-podium-display-background_1101917-13418.jpg?w=900",
                       ),
                     ),
+                    child: Text(
+                      "${productModel.sale}% OFF",
+                      style: const TextStyle(color: AppColors.kWhiteColor),
+                    ),
                   ),
-                  Positioned(
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 65,
-                      height: 35,
-                      decoration: const BoxDecoration(
-                          color: AppColors.kPrimaryColor,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(16),
-                            bottomRight: Radius.circular(16),
-                          )),
-                      child: Text(
-                        "${productModel.sale}% OFF",
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        productModel.productName ?? "Product Name",
                         style: const TextStyle(
-                          color: AppColors.kWhiteColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
+                      IconButton(
+                        onPressed: onPressed,
+                        icon: Icon(
+                          Icons.favorite,
+                          color: isFavourite
+                              ? AppColors.kPrimaryColor
+                              : Colors.grey,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
                         children: [
                           Text(
-                            productModel.productName ?? "Product Name",
+                            "${productModel.price} LE",
                             style: const TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          IconButton(
-                              onPressed: onPressed,
-                              icon: Icon(
-                                Icons.favorite,
-                                color: isFavourite
-                                    ? AppColors.kPrimaryColor
-                                    : Colors.grey,
-                              ))
-                        ]),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "${productModel.price} LE",
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+                          Text(
+                            "${productModel.oldPrice} LE",
+                            style: const TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.kGreyColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      CustomEBtn(
+                        text: "Buy Now",
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PaymentView(
+                                onPaymentSuccess: () {
+                                  log('Payment Success');
+                                },
+                                onPaymentError: () {
+                                  // Handle payment failure
+
+                                  log('Payment Failure');
+                                },
+                                price: double.parse(
+                                  productModel.price!,
+                                ), // Required: Total price (e.g., 100 for 100 EGP)
                               ),
                             ),
-                            Text(
-                              "${productModel.oldPrice} LE",
-                              style: const TextStyle(
-                                decoration: TextDecoration.lineThrough,
-                                fontWeight: FontWeight.bold,
-                                color: AppColors.kGreyColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                        CustomEBtn(
-                          text: "Buy Now",
-                          onTap: () {},
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:our_market/core/app_colors.dart';
 import 'package:our_market/core/components/custom_search_field.dart';
 import 'package:our_market/core/components/products_list.dart';
 import 'package:our_market/core/cubit/home_cubit.dart';
 import 'package:our_market/core/functions/navigate_to.dart';
 import 'package:our_market/views/home/ui/search_view.dart';
 import 'package:our_market/views/home/ui/widgets/categories_list.dart';
+import 'package:our_market/views/store/sensetive.dart';
+import 'package:pay_with_paymob/pay_with_paymob.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,6 +21,41 @@ class _HomeViewState extends State<HomeView> {
   final TextEditingController _searchController = TextEditingController();
   final FocusNode _searchFocusNode = FocusNode();
   String? query;
+  @override
+  void initState() {
+    PaymentData.initialize(
+      apiKey:
+          apiKey, // Required: Found under Dashboard -> Settings -> Account Info -> API Key
+      iframeId: iframeId, // Required: Found under Developers -> iframes
+      integrationCardId:
+          integrationId, // Required: Found under Developers -> Payment Integrations -> Online Card ID
+      integrationMobileWalletId:
+          integrationMobileWalletId, // Required: Found under Developers -> Payment Integrations -> Mobile Wallet ID
+      // // Optional User Data
+      // userData: UserData(
+      //   email: "User Email", // Optional: Defaults to 'NA'
+      //   phone: "User Phone", // Optional: Defaults to 'NA'
+      //   name: "User First Name", // Optional: Defaults to 'NA'
+      //   lastName: "User Last Name", // Optional: Defaults to 'NA'
+      // ),
+
+      // Optional Style Customizations
+      style: Style(
+        primaryColor: AppColors.kPrimaryColor, // Default: Colors.blue
+        scaffoldColor: Colors.white, // Default: Colors.white
+        appBarBackgroundColor: AppColors.kPrimaryColor, // Default: Colors.blue
+        appBarForegroundColor: Colors.white, // Default: Colors.white
+
+        buttonStyle: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.kPrimaryColor,
+          textStyle: const TextStyle(color: AppColors.kWhiteColor),
+        ), // Default: ElevatedButton.styleFrom()
+        circleProgressColor: AppColors.kPrimaryColor, // Default: Colors.blue
+        unselectedColor: Colors.grey, // Default: Colors.grey
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,6 +129,7 @@ class _HomeViewState extends State<HomeView> {
   @override
   void dispose() {
     _searchController.dispose();
+
     _searchFocusNode.dispose();
     super.dispose();
   }
